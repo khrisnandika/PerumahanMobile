a434fe388df4bd987a3d85464cf09d37637437be
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2023-04-01 22:54:01 +0700

    add navbar and ui profile layout

--- app/src/main/java/com/example/perumahan/MainActivity.java
+++ app/src/main/java/com/example/perumahan/MainActivity.java
@@ -1,14 +1,48 @@
 package com.example.perumahan;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationBarView;
 
 public class MainActivity extends AppCompatActivity {
+
+    BottomNavigationView bottomNavigationView;
+
+    HomeFragment homeFragment = new HomeFragment();
+    UploadFragment uploadFragment = new UploadFragment();
+    ProfileFragment profileFragment = new ProfileFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        bottomNavigationView = findViewById(R.id.bottom_navigation);
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+
+        bottomNavigationView.setOnItemSelectedListener(new NavigationBarView.OnItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.home:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, homeFragment).commit();
+                        return true;
+                    case R.id.upload:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, uploadFragment).commit();
+                        return true;
+                    case R.id.profile:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.container, profileFragment).commit();
+                        return true;
+
+                }
+                return false;
+            }
+        });
     }
 }
