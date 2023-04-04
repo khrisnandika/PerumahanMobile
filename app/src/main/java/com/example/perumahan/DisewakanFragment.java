83975c96dbc941a839fb386b291114c6a6031442
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2023-04-04 23:28:06 +0700

    add home cardView and RecyclerView

--- app/src/main/java/com/example/perumahan/DisewakanFragment.java
+++ app/src/main/java/com/example/perumahan/DisewakanFragment.java
@@ -3,19 +3,51 @@ package com.example.perumahan;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.util.ArrayList;
+
 
 public class DisewakanFragment extends Fragment {
 
+    private RecyclerView recyclerView;
+    private AdapterRumahSewa adapterRumahSewa;
+    private ArrayList<ModelRumahSewa> modelRumahSewa;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_disewakan, container, false);
+        View view = inflater.inflate(R.layout.fragment_disewakan, container, false);
+
+        getData();
+
+        recyclerView = view.findViewById(R.id.rumahsewaRecycler);
+        adapterRumahSewa = new AdapterRumahSewa(modelRumahSewa);
+
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapterRumahSewa);
+
+        return view;
+    }
+
+    private void getData(){
+        modelRumahSewa = new ArrayList<>();
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Berseri","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Enjoy","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Slepek","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Pcc","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Sibaw","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Cuak","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah YOYOYO","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Tess","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Berseri","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
+        modelRumahSewa.add(new ModelRumahSewa("Rumah Berseri","Status : Tersedia","Jalan Jendral Sudirman", R.drawable.rumah1));
     }
 }
