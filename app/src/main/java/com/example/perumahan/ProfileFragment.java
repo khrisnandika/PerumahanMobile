7361b861db948611e937f521dfd0346e878ce43a
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2023-04-06 01:04:05 +0700

    update ui profile

--- app/src/main/java/com/example/perumahan/ProfileFragment.java
+++ app/src/main/java/com/example/perumahan/ProfileFragment.java
@@ -1,5 +1,6 @@
 package com.example.perumahan;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,14 +8,29 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class ProfileFragment extends Fragment {
 
+
+    Button editBtn;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        editBtn = view.findViewById(R.id.btnEdit);
+
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), EditProfile.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
     }
 }
