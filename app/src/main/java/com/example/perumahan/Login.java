dcd20328cabdfa0299bf47b747c0ebf0b2243444
IndraPras <indrapras2002@gmail.com>
2023-04-08 00:20:56 +0700

    add logic auth

--- app/src/main/java/com/example/perumahan/Login.java
+++ app/src/main/java/com/example/perumahan/Login.java
@@ -2,13 +2,31 @@ package com.example.perumahan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Login extends AppCompatActivity {
-
+EditText username,password;
+Button Blog;
+TextView Textreg;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Textreg = (TextView) findViewById(R.id.Treg);
+        username = (EditText) findViewById(R.id.txtField1);
+        username = (EditText) findViewById(R.id.txtField2);
+
+        Textreg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent registerIntent = new Intent(Login.this, Register.class);
+                startActivity(registerIntent);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/perumahan/Register.java
+++ app/src/main/java/com/example/perumahan/Register.java
@@ -2,13 +2,131 @@ package com.example.perumahan;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class Register extends AppCompatActivity {
-
+    EditText username,email,password,confirmPass;
+    TextView TextLog;
+    Button Bregister;
+    ProgressDialog progressDialog;
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        username = (EditText) findViewById(R.id.txtField1);
+        email = (EditText) findViewById(R.id.txtField2);
+        password = (EditText) findViewById(R.id.txtField3);
+        confirmPass = (EditText) findViewById(R.id.txtField4);
+        TextLog = (TextView) findViewById(R.id.TextLog);
+        Bregister = (Button) findViewById(R.id.Tregister);
+        progressDialog = new ProgressDialog(Register.this);
+
+        TextLog.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent loginIntent = new Intent(Register.this, Login.class);
+                startActivity(loginIntent);
+            }
+        });
+
+        Bregister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String sUsername = username.getText().toString();
+                String sEmail = email.getText().toString();
+                String sPassword = password.getText().toString();
+                String sConfPassword = confirmPass.getText().toString();
+
+                if (sPassword.equals(sConfPassword) && !sPassword.equals("")) {
+                    CreateDataToServer(sUsername, sEmail, sPassword);
+                    Intent loginIntent = new Intent(Register.this, Login.class);
+                    startActivity(loginIntent);
+                } else {
+                    Toast.makeText(getApplicationContext(), "Gagal! Pasword tidak cocok!", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
+
+        public void CreateDataToServer(final String username, final String email, final String password) {
+            if (checkNetworkConnection()) {
+                progressDialog.show();
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, DbContract.SERVER_REGISTER_URL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                try {
+                                    JSONObject jsonObject = new JSONObject(response);
+                                    String resp = jsonObject.getString("server_response");
+                                    if (resp.equals("[{\"status\":\"OK\"}]")) {
+                                        Toast.makeText(getApplicationContext(), "Registrasi Berhasil", Toast.LENGTH_SHORT).show();
+                                        Intent loginIntent = new Intent(Register.this, Login.class);
+                                        startActivity(loginIntent);
+                                    } else {
+                                        Toast.makeText(getApplicationContext(), resp, Toast.LENGTH_SHORT).show();
+                                    }
+                                } catch (JSONException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }) {
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String, String> params = new HashMap<>();
+                        params.put("username", username);
+                        params.put("email", email);
+                        params.put("password", password);
+                        return params;
+                    }
+                };
+
+                VolleyConnection.getInstance(Register.this).addToRequestQue(stringRequest);
+
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        progressDialog.cancel();
+                    }
+                }, 2000);
+            } else {
+                Toast.makeText(getApplicationContext(), "Tidak ada koneksi internet", Toast.LENGTH_SHORT).show();
+            }
+        }
+    public boolean checkNetworkConnection() {
+        ConnectivityManager connectivityManager = (ConnectivityManager) this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+        return (networkInfo != null && networkInfo.isConnected());
+    }
+
 }
