699928a6ac6ce4f36869724dd462a9741c587cdb
Krisna Choiril Andika <99785580+khrisnandika@users.noreply.github.com>
2023-04-08 21:47:45 +0700

    update profile

--- app/src/main/java/com/example/perumahan/ProfileFragment.java
+++ app/src/main/java/com/example/perumahan/ProfileFragment.java
@@ -9,11 +9,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.RelativeLayout;
 
 public class ProfileFragment extends Fragment {
 
 
     Button editBtn;
+    RelativeLayout btnKataSandi;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -22,6 +24,15 @@ public class ProfileFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         editBtn = view.findViewById(R.id.btnEdit);
+        btnKataSandi = view.findViewById(R.id.btnKataSandi);
+
+        btnKataSandi.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), KataSandi.class);
+                startActivity(intent);
+            }
+        });
 
         editBtn.setOnClickListener(new View.OnClickListener() {
             @Override
