b496f0b3a3d4a3b78ef3e4828586738460622225
Angga Raksa <91731031+AnggaRaksa009@users.noreply.github.com>
2023-05-22 23:00:32 +0700

    update databasehelepr

--- app/src/main/java/com/example/perumahan/DatabaseHelper.java
+++ app/src/main/java/com/example/perumahan/DatabaseHelper.java
@@ -4,6 +4,18 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String DATABASE_NAME = "api";
@@ -11,6 +23,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     private static final String COL_ID = "id";
     private static final String COL_USERNAME = "username";
     private static final String COL_PASSWORD = "password";
+    private static final String API_URL = "http://10.0.2.2:80/api/tampil_data.php";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -35,4 +48,72 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         SQLiteDatabase db = this.getWritableDatabase();
         return db.delete(TABLE_NAME, COL_ID + "=?", new String[]{String.valueOf(id)}) > 0;
     }
+
+    public void fetchDataFromAPI() {
+        new FetchDataTask().execute(API_URL);
+    }
+
+    private class FetchDataTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection connection = null;
+
+            try {
+                URL url = new URL(urls[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.setRequestMethod("GET");
+
+                InputStream inputStream = connection.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+                StringBuilder stringBuilder = new StringBuilder();
+                String line;
+
+                while ((line = reader.readLine()) != null) {
+                    stringBuilder.append(line);
+                }
+
+                response = stringBuilder.toString();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+            }
+
+            return response;
+        }
+
+        @Override
+        protected void onPostExecute(String response) {
+            SQLiteDatabase db = getWritableDatabase();
+
+            try {
+                JSONArray jsonArray = new JSONArray(response);
+
+
+                db.delete(TABLE_NAME, null, null);
+
+                for (int i = 0; i < jsonArray.length(); i++) {
+                    JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                    String username = jsonObject.getString("username");
+                    String password = jsonObject.getString("password");
+
+                    ContentValues values = new ContentValues();
+                    values.put(COL_USERNAME, username);
+                    values.put(COL_PASSWORD, password);
+
+                    db.insert(TABLE_NAME, null, values);
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } finally {
+                if (db != null) {
+                    db.close();
+                }
+            }
+        }
+        }
 }
