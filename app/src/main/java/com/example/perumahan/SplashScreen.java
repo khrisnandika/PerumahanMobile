469a5ddb128f854044a98a09c9d7a57351c5e930
Angga Raksa <91731031+AnggaRaksa009@users.noreply.github.com>
2023-05-28 23:19:20 +0700

    add splash

---
+++ app/src/main/java/com/example/perumahan/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.perumahan;
+
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import com.example.perumahan.MainActivity;
+import com.example.perumahan.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //menghilangkan ActionBar
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_motion);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                finish();
+            }
+        }, 2000L);
+    }
+}
